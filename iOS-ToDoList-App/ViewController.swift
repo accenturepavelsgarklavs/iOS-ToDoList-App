16d108099643ba4a7efc7a48b1c8760c32019dc7
Pāvels Garklāvs <pavels.garklavs@accenture.com>
2022-02-17 09:34:23 +0200

    Initial Commit

---
+++ iOS-ToDoList-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-ToDoList-App
+//
+//  Created by pavels.garklavs on 17/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
